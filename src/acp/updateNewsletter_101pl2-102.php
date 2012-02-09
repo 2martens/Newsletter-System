41c08c0302fdd4192a403f641100d040fc7615d1
Jim Martens <jim1@live.de>
2012-02-09 19:33:11 +0100

    Fixed problem with update script and checkbox problem.

--- src/acp/updateNewsletter_101pl2-102.php
+++ src/acp/updateNewsletter_101pl2-102.php
@@ -2,4 +2,4 @@
 //wcf imports
 require_once(WCF_DIR.'lib/system/cache/CacheHandler.class.php');
 
-CacheHandler::clear(WCF_DIR.'cache', 'cache.newsletter-*.php');
+WCF::getCache()->clear(WCF_DIR.'cache', 'cache.newsletter-*.php');
